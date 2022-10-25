87f3954d722994aa14c5987302957a5c50d50092
DanielBrandao12 <drb12da@gmail.com>
2022-10-25 18:22:09 -0300

    função

--- index.js
+++ index.js
@@ -1,22 +1,41 @@
 let cont = 0
 
-
-const board = document.querySelectorAll('.box')
-
-const positions = [1,2,3,4,5,6,7,8,""]
- 
+const board = document.querySelector('.board')
 
 
-board.forEach( buttons=>{
+let positions = [1,2,3,4,5,6,7,8,""]
 
-  const positions = [1,2,3,4,5,6,7,8,""]
+let arr = []
 
-  board[buttons.id-1].innerText = positions[buttons.id-1]
+
+
+
+
+for(let i =0; i<positions.length;i++){
+
+  board.innerHTML += `<div class="box" id="${i+1}">${positions[i]}</div>`
+  arr.push(board.innerHTML)
+  
+  
+}
+console.log(arr)
+
+const box = document.querySelectorAll('.box')
+box.forEach( buttons=>{
+
+
+  box[buttons.id-1].innerText = positions[buttons.id-1]
 
   buttons.addEventListener('click', event=>{
-   
-  
+      console.log(buttons)
+    if(buttons.innerText == '6'){
+        arr[5] =''
+        arr[9] ='6'
+       box[buttons.id-1].innerText =""
+      box[8].innerText='6'
+    }
+    console.log(arr)
   })
 })
 
-//console.log(board.innerHTML.valueOf())
+//console.log(box.innerHTML.valueOf())
