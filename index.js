1b9f9752dbb10b85004e4e9b94af94992ee9dab1
DanielBrandao12 <drb12da@gmail.com>
2023-05-24 19:17:33 -0300

    lógica de comparar as casas que podem ser trocadas

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const board = document.querySelector('.board')
 const btnEmbaralhar = document.querySelector('.embaralhar')
-
+const win = document.querySelector('.divWin')
 let positions = [1, 2, 3, 4, 5, 6, 7, 8, ""]
 let arr = []
 let ganha = ['1', '2', '3', '4', '5', '6', '7', '8', '']
@@ -39,22 +39,35 @@ function criarTabuleiro() {
       for (let i = 0; i < box.length; i++) {
 
         if (box[i].innerText == '') {
+          console.log(event.target.id)
+          console.log(box[i].id)
+          if(event.target.id%2 == 1 && box[i].id%2==0){
 
+           
           box[i].innerText = buttons.innerText
           arr[arr.indexOf(buttons.innerText)] = ""
           arr[i] = buttons.innerText
+          buttons.innerText = ""
+          }else if(event.target.id%2 == 0 && box[i].id%2==1){
+            
+          box[i].innerText = buttons.innerText
+          arr[arr.indexOf(buttons.innerText)] = ""
+          arr[i] = buttons.innerText
+          buttons.innerText = ""
+          }
 
         }
       }
-      buttons.innerText = ""
+      
 
      
 
       console.log(arr)
 
-      console.log(ganha)
-       if (arr == ganha) {
+     
+       if (JSON.stringify(arr) == JSON.stringify(ganha)) {
     console.log("voce ganhou")
+    win.style.display = 'flex'
   }
     })
   })
